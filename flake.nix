e31b0edf7398014baf7eaf195aaf3f7cf513cc27
felixg <felix.guenther@iu-study.org>
2024-06-24 19:40:56 +0200

    removed pip from flake.nix added python dependencies to flake.nix

--- flake.nix
+++ flake.nix
@@ -15,18 +15,28 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        python = pkgs.python310; # Use the desired Python version
       in
       {
         devShell = pkgs.mkShell {
           # List your development dependencies here
           buildInputs = [
-            pkgs.python3
-            pkgs.python3Packages.pip
+            python
             pkgs.git
             pkgs.nodejs
+            pkgs.virtualenv
           ];
 
-          # Any additional shell setup can go here
+          # Define Python packages to install
+          PYTHON_PACKAGES = [
+            python.pkgs.requests # example package, replace with your requirements
+            python.pkgs.flask
+            python.pkgs.numpy
+            python.pkgs.BeautifulSoup4
+            python.pkgs.sqlalchemy
+            python.pkgs.pdfminer
+          ];
+
           shellHook = ''
             export PROJECT_PATH=/home/felixg/python_projects/unicrawler-react
             cd $PROJECT_PATH
@@ -36,6 +46,16 @@
             else
               echo "No Git repository found in $PROJECT_PATH"
             fi
+
+            # Create and activate virtual environment
+            if [ ! -d ".venv" ]; then
+              virtualenv .venv
+            fi
+            source .venv/bin/activate
+
+            # Install Python packages
+            pip install ${builtins.concatStringsSep " " (map (p: "${p.outPath}") PYTHON_PACKAGES)}
+            echo "Virtual environment activated and packages installed."
           '';
         };
       });
