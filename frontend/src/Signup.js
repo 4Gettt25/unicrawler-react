e1bd649b308e880e90f8a1f57bcd8dfa7774f1c8
felixg <felix.guenther@iu-study.org>
2024-07-15 22:21:22 +0200

    chore: Update Login and Signup CSS
    This commit updates the Login.css and Signup.css files to improve the styling and layout of the login and signup forms.
    
    In Login.css, the padding for the login form button is commented out, removing the padding. This change may have been made to adjust the button's appearance or alignment.
    
    In Signup.css, the padding for the signup form is increased to 30px, and a fixed width of 360px is set for the form. Additionally, the text-align property is set to center for the form and the h2 heading. These changes enhance the visual presentation and alignment of the signup form.
    
    These CSS updates improve the user experience and visual consistency of the login and signup pages.

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -16,26 +16,33 @@ const Signup = () => {
 			<div className="signup-container">
 				<form className="signup-form" onSubmit={handleSignup}>
 					<h2>Sign up to Unicrawler</h2>
+					<p>
+						Unicrawler does not make any external connections, and your data
+						stays securely on your locally hosted server.
+					</p>
 					<label>Name</label>
 					<input
 						type="text"
+						placeholder="Enter Your Full Name"
 						value={name}
 						onChange={(e) => setName(e.target.value)}
 					/>
 					<label>Email</label>
 					<input
 						type="email"
+						placeholder="Enter Your Email"
 						value={email}
 						onChange={(e) => setEmail(e.target.value)}
 					/>
 					<label>Password</label>
 					<input
 						type="password"
+						placeholder="Enter Your Password"
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 					/>
 					<button type="submit">Create Account</button>
-					<p>
+					<p className="sign-in-link">
 						Already have an account? <a href="/login">Sign in</a>
 					</p>
 				</form>
